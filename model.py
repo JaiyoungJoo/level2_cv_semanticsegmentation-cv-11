76a41de6cfae429de465f8325397f602a0da3b5c
junha-lee <junha4304@gmail.com>
2023-06-13 18:49:27 +0000

    [FEAT] HRNet, MultimodalV2

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torchvision.models import segmentation
 import segmentation_models_pytorch as smp
 import torch
+import hrnet
+import torch.nn.functional as F
 
 
 
@@ -50,6 +52,19 @@ class FPN(nn.Module):
         out = self.net(x)
         return out
     
+
+# hrnet
+class  HRNet(nn.Module):
+    def __init__(self, name='hrnet48',pretrained ="/opt/ml/weights/hrnetv2_w48_imagenet_pretrained.pth",num_classes=29, encoder = 'HR'):
+        super().__init__()
+        self.net = hrnet.get_ocr_model(name = name, pretrained=pretrained)
+    
+    def forward(self, x):
+        out = self.net(x)
+        out = F.interpolate(out, size=(512, 512), mode="bilinear")
+        return out
+    
+
 # pretrained weight
 
 class Pretrained_torchvision(nn.Module):
@@ -144,5 +159,91 @@ class MultiModal(nn.Module):
         out_weight = self.branch_weight(out_flatten)
         out_hight = self.branch_hight(out_flatten)
 
+        
+        return  out_segment, out_age, out_gender, out_weight, out_hight
+    
+class MultiModalV2(nn.Module):
+    def __init__(self, in_features=29, encoder = 'densenet169'):
+        super().__init__()
+
+        self.backbone = hrnet.get_ocr_model(name='hrnet48',pretrained ="/opt/ml/weights/hrnetv2_w48_imagenet_pretrained.pth")
+
+        self.age_fc = nn.Linear(1, in_features)
+        self.gender_fc = nn.Linear(2, in_features)
+        self.weight_fc = nn.Linear(1, in_features)
+        self.hight_fc = nn.Linear(1, in_features)
+        
+        # self.downsampling = nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1)
+
+        self.branch_age = nn.Sequential(
+                nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1),
+                nn.Flatten(),
+                nn.Linear(in_features=19604, out_features=51),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.3),
+                nn.Linear(51,1),
+                nn.Softmax()
+            )
+        self.branch_gender = nn.Sequential(
+                nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1),
+                nn.Flatten(),
+                nn.Linear(in_features=19604, out_features=52),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.3),
+                nn.Linear(52,2),
+                nn.Softmax()
+            )
+        self.branch_weight = nn.Sequential(
+                nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1),
+                nn.Flatten(),
+                nn.Linear(in_features=19604, out_features=51),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.3),
+                nn.Linear(51,1),
+                nn.Softmax()
+            )
+        self.branch_hight = nn.Sequential(
+                nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1),
+                nn.Flatten(),
+                nn.Linear(in_features=19604, out_features=51),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.3),
+                nn.Linear(51,1),
+                nn.Softmax()
+            )
+
+
+        self.decoder = nn.Sequential(
+            # Define the decoder layers, such as upsampling and transposed convolutions
+            nn.ConvTranspose2d(in_features*5, in_features*3, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(in_features*3, in_features*2, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_features*2, in_features, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_features, 29, kernel_size=1, stride=1),
+        )
+    def forward(self, x, age, gender, weight, hight):
+        out = self.backbone(x)
+        age = self.age_fc(age)
+
+        gender = self.gender_fc(gender)
+
+        weight = self.weight_fc(weight)
+        hight = self.hight_fc(hight)
+
+        out_all = torch.cat([out, age.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
+        out_all = torch.cat([out_all, gender.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
+        out_all = torch.cat([out_all, weight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
+        out_all = torch.cat([out_all, hight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
+
+        out_segment = self.decoder(out_all)
+        #out_down = self.downsampling(out)
+        #out_flatten = out_down.view(out_down.size(0),-1)
+        
+        out_age = self.branch_age(out) # out_flatten -> out
+        out_gender = self.branch_gender(out) # out_flatten -> out
+        out_weight = self.branch_weight(out) # out_flatten -> out
+        out_hight = self.branch_hight(out) # out_flatten -> out
         
         return  out_segment, out_age, out_gender, out_weight, out_hight
