59e6b306740fe113bafbfe9af654de2a071dc155
junha-lee <junha4304@gmail.com>
2023-06-18 18:59:21 +0000

    [FEAT] train_multimodal update

--- train_multimodal.py
+++ train_multimodal.py
@@ -3,7 +3,7 @@ import random
 import datetime
 import argparse
 from importlib import import_module
-
+import ssl
 # external library
 import numpy as np
 from tqdm.auto import tqdm
@@ -15,16 +15,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Subset
-
 # visualization
 import wandb
 
 # dataset
 from dataset import XRayDataset_Multi
+from dataset import get_transform
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# for time check
+import time
+from pytz import timezone
 
 
-# exp setting
-BATCH_SIZE = 8
 LR = 1e-4
 CLASSES = [
     'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
@@ -34,17 +37,26 @@ CLASSES = [
     'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
     'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
 ]
+my_table = wandb.Table(
+    columns=["epoch"] + CLASSES)
 
 def check_path(path):
     # 가중치 저장 경로 설정
     if not os.path.isdir(path):                                                           
-        os.mkdir(path)
+        os.makedirs(path)
 
 def make_dataset(debug="False"):
     # dataset load
     tf = A.Resize(512, 512)
-    train_dataset = XRayDataset_Multi(is_train=True, transforms=tf)
-    valid_dataset = XRayDataset_Multi(is_train=False, transforms=tf)
+    train_transform, val_transform = get_transform()
+    if args.transform=='True':
+        train_dataset = XRayDataset_Multi(is_train=True, transforms=train_transform, dataclean=args.dataclean)
+        valid_dataset = XRayDataset_Multi(is_train=False, transforms=val_transform, dataclean=args.dataclean)
+    else:
+        train_dataset = XRayDataset_Multi(is_train=True, transforms=tf, dataclean=args.dataclean)
+        valid_dataset = XRayDataset_Multi(is_train=False, transforms=tf, dataclean=args.dataclean)
+ 
+ 
     if debug=="True":
         train_subset_size = int(len(train_dataset) * 0.1)
 
@@ -61,12 +73,11 @@ def make_dataset(debug="False"):
         
     train_loader = DataLoader(
         dataset=train_dataset, 
-        batch_size=BATCH_SIZE,
+        batch_size=args.train_batch,
         shuffle=True,
-        num_workers=8,
+        num_workers=args.train_workers,
         drop_last=True,
     )
-
     valid_loader = DataLoader(
         dataset=valid_dataset, 
         batch_size=2,
@@ -100,13 +111,13 @@ def set_seed(seed):
     random.seed(seed)
 
 def wandb_config(args):
-    wandb.init(config={'batch_size':BATCH_SIZE,
+    wandb.init(config={'batch_size':args.train_batch,
                     'learning_rate':LR,                 #차차 args.~~로 update할 것
                     'seed':args.seed,
                     'max_epoch':args.epochs},
             project='Segmentation',
             entity='aivengers_seg',
-            name=f'{args.model}_{args.encoder}_{args.epochs}'
+            name=f'{args.model}_{args.encoder}_{args.loss}_tf={args.transform}_cln={args.dataclean}_e={args.epochs}_sd={args.seed}'
             )
 
 def validation(epoch, model, data_loader, criterion, thr=0.5):
@@ -119,11 +130,11 @@ def validation(epoch, model, data_loader, criterion, thr=0.5):
         total_loss = 0
         cnt = 0
 
-        for step, (images, masks, ages, genders, weights, hights) in tqdm(enumerate(data_loader), total=len(data_loader)):
-            images, masks, ages, genders, weights, hights = images.cuda(), masks.cuda(), ages.cuda(), genders.cuda(), weights.cuda(), hights.cuda()     
+        for step, (images, masks, ages, genders, weights, heights) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            images, masks, ages, genders, weights, heights = images.cuda(), masks.cuda(), ages.cuda(), genders.cuda(), weights.cuda(), heights.cuda()     
             model = model.cuda()
 
-            outputs, ages_, genders_, weights_, hights_ = model(images, ages, genders, weights, hights)
+            outputs, ages_, genders_, weights_, heights_ = model(images, ages, genders, weights, heights)
             
             output_h, output_w = outputs.size(-2), outputs.size(-1)
             mask_h, mask_w = masks.size(-2), masks.size(-1)
@@ -136,9 +147,9 @@ def validation(epoch, model, data_loader, criterion, thr=0.5):
             age_criterion =  getattr(import_module("loss"), 'mse_loss')(ages_, ages)
             genders_criterion =  getattr(import_module("loss"), 'bce_loss')(genders_, genders)
             weights_criterion =  getattr(import_module("loss"), 'mse_loss')(weights_, weights)
-            hights_criterion =  getattr(import_module("loss"), 'mse_loss')(hights_, hights)
+            heights_criterion =  getattr(import_module("loss"), 'mse_loss')(heights_, heights)
 
-            loss = segmentation_criterion*0.5 + age_criterion*0.1 + genders_criterion*0.1 + weights_criterion*0.1 + hights_criterion*0.1
+            loss = segmentation_criterion*0.5 + age_criterion*0.1 + genders_criterion*0.1 + weights_criterion*0.1 + heights_criterion*0.1
             total_loss += loss
             cnt += 1
             
@@ -182,38 +193,39 @@ def train(model, data_loader, val_loader, criterion, optimizer, args):
 
         model.train()
 
-        for step, (images, masks, ages, genders, weights, hights) in enumerate(data_loader):            
+        for step, (images, masks, ages, genders, weights, heights) in enumerate(data_loader):            
             # gpu 연산을 위해 device 할당
-            images, masks, ages, genders, weights, hights = images.cuda(), masks.cuda(), ages.cuda(), genders.cuda(), weights.cuda(), hights.cuda()
+            images, masks, ages, genders, weights, heights = images.cuda(), masks.cuda(), ages.cuda(), genders.cuda(), weights.cuda(), heights.cuda()
             model = model.cuda()
             
             # inference
-            outputs, ages_, genders_, weights_, hights_ = model(images, ages, genders, weights, hights)
+            outputs, ages_, genders_, weights_, heights_ = model(images, ages, genders, weights, heights)
 
             # loss 계산
             segmentation_criterion = criterion(outputs, masks)
             age_criterion =  getattr(import_module("loss"), 'mse_loss')(ages_, ages)
             genders_criterion =  getattr(import_module("loss"), 'bce_loss')(genders_, genders)
             weights_criterion =  getattr(import_module("loss"), 'mse_loss')(weights_, weights)
-            hights_criterion =  getattr(import_module("loss"), 'mse_loss')(hights_, hights)
+            heights_criterion =  getattr(import_module("loss"), 'mse_loss')(heights_, heights)
 
-            loss = segmentation_criterion*0.5 + age_criterion*0.1 + genders_criterion*0.1 + weights_criterion*0.1 + hights_criterion*0.1
-            
-            
-            #print(f'age_pred: {ages_} ')
-            #print(f'age_label: {ages.unsqueeze(1)} ')
-            #print(f'gender_pred: {genders_} ')
-            #print(f'gender_label: {genders.unsqueeze(1)} ')
-            #print(f'weight_pred: {weights_} ')
-            #print(f'weight_label: {weights.unsqueeze(1)} ')
-            #print(f'hight_pred: {hights_} ')
-            #print(f'hight_label: {hights.unsqueeze(1)} ')
-                
-            
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            loss = segmentation_criterion + age_criterion*0.1 + genders_criterion*0.1 + weights_criterion*0.1 + heights_criterion*0.1
+            if args.acc_steps == 'False':
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            else:
+                args.acc_steps = int(args.acc_steps)
+                loss = loss / args.acc_steps # 경사를 경사 누적 스텝 수로 나눔
+                loss.backward()
+                step_count += 1
+                if step_count % args.acc_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    step_count = 0
+                # 경사 업데이트 없이 스텝을 끝마치기 전에 경사 초기화
+                if step_count != 0 and (step_count % args.acc_steps) != 0:
+                    optimizer.zero_grad()
             
             # step 주기에 따른 loss 출력
             if (step + 1) % 20 == 0:
@@ -226,14 +238,14 @@ def train(model, data_loader, val_loader, criterion, optimizer, args):
                     f'age_Loss: {round(age_criterion.item(),4)} '
                     f'gender_Loss: {round(genders_criterion.item(),4)} '
                     f'weight_Loss: {round(weights_criterion.item(),4)} '
-                    f'hight_Loss: {round(hights_criterion.item(),4)} '
+                    f'height_Loss: {round(heights_criterion.item(),4)} '
                 )
             train={'Loss':round(loss.item(),4),
                    'seg_Loss': round(segmentation_criterion.item(),4),
                     'age_Loss': round(age_criterion.item(),4),
                     'gender_Loss': round(genders_criterion.item(),4),
                     'weight_Loss': round(weights_criterion.item(),4),
-                    'hight_Loss': round(hights_criterion.item(),4)}
+                    'height_Loss': round(heights_criterion.item(),4)}
             
             if args.wandb=="True":
                 wandb.log(train, step = epoch)
@@ -252,6 +264,8 @@ def train(model, data_loader, val_loader, criterion, optimizer, args):
                  'best_dice':best_dice}
             if args.wandb=="True":
                 wandb.log(val, step = epoch)
+
+    wandb.log({"Table Name": my_table}, step=epoch)
 
 
 def main(args):
@@ -272,7 +286,7 @@ def main(args):
         set_seed(args.seed)
         train_loader, valid_loader = make_dataset(args.debug)
     else:
-        set_seed(0)
+        set_seed(args.seed)
         train_loader, valid_loader = make_dataset()
 
     check_path(args.save_dir)
@@ -281,16 +295,21 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", default=21, help="random seed (default: 21)")
-    parser.add_argument("--loss", type=str, default="bce_loss")
-    parser.add_argument("--model", type=str, default="MultiModalV2")
-    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=70, help="random seed (default: 0), select one of [0,1,2,3,4]")
+    parser.add_argument("--loss", type=str, default="comb_loss")
+    parser.add_argument("--model", type=str, default="MultiModalV3")
+    parser.add_argument("--epochs", type=int, default=200)
     parser.add_argument("--val_every", type=int, default=1)
+    parser.add_argument("--train_batch", type=int, default=3, help = 'image size 1024 - batch 4 이하, 512 - 8이하')
+    parser.add_argument("--train_workers", type=int, default=4, help = 'default = 4 (==batch_size)')
     parser.add_argument("--wandb", type=str, default="True")
-    parser.add_argument("--encoder", type=str, default="hrnet")
-    parser.add_argument("--save_dir", type=str, default="/opt/ml/weights/")
-    parser.add_argument("--model_path", type=str, default="/opt/ml/weights/MultiModalV2/MultiModalV2_hrnet_100.pt")
+    parser.add_argument("--encoder", type=str, default="HR")
+    parser.add_argument("--save_dir", type=str, default="/opt/ml/input/weights/")
+    parser.add_argument("--model_path", type=str, default="/opt/ml/weights/fcn_resnet101_best_model.pt")
     parser.add_argument("--debug", type=str, default="False")
+    parser.add_argument("--transform",type=str, default="True")
+    parser.add_argument("--acc_steps", type=str, default="False")
+    parser.add_argument("--dataclean",type=str, default="True")
 
 
     args = parser.parse_args()
@@ -299,5 +318,12 @@ if __name__ == '__main__':
     else:
         args.save_dir = os.path.join(args.save_dir, args.model)
 
+    start = time.time()
+    print(f"Model save dir: {args.save_dir}")
     print(args)
     main(args)
+    end = time.time()
+    sec = (end - start)
+    result = datetime.timedelta(seconds=sec)
+    result_list = str(datetime.timedelta(seconds=sec)).split(".")
+    print(result_list[0])
