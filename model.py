9ebc7a88c85c817528aad4ef3e908cec5f3238a2
junha-lee <junha4304@gmail.com>
2023-06-19 07:08:22 +0000

    [FEAT] softmzx -> sigmoid

--- model.py
+++ model.py
@@ -117,9 +117,9 @@ class Pretrained_Multimodal(nn.Module):
         super().__init__()
         self.net = torch.load(model)
 
-    def forward(self, x, age, gender, weight, hight):
-        out_segment, out_age, out_gender, out_weight, out_hight = self.net(x, age, gender, weight, hight)
-        return out_segment, out_age, out_gender, out_weight, out_hight
+    def forward(self, x, age, gender, weight, height):
+        out_segment, out_age, out_gender, out_weight, out_height = self.net(x, age, gender, weight, height)
+        return out_segment, out_age, out_gender, out_weight, out_height
     
 
 # multi modal
@@ -132,7 +132,7 @@ class MultiModal(nn.Module):
         self.age_fc = nn.Linear(1, in_features)
         self.gender_fc = nn.Linear(1, in_features)
         self.weight_fc = nn.Linear(1, in_features)
-        self.hight_fc = nn.Linear(1, in_features)
+        self.height_fc = nn.Linear(1, in_features)
         
         self.downsampling = nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1)
 
@@ -157,7 +157,7 @@ class MultiModal(nn.Module):
                 nn.Linear(51,1),
                 nn.Softmax()
             )
-        self.branch_hight = nn.Sequential(
+        self.branch_height = nn.Sequential(
                 nn.Linear(in_features=312120, out_features=51),
                 nn.ReLU(inplace=True),
                 nn.Dropout(0.3),
@@ -174,17 +174,17 @@ class MultiModal(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(in_features, 29, kernel_size=1, stride=1),
         )
-    def forward(self, x, age, gender, weight, hight):
+    def forward(self, x, age, gender, weight, height):
         out = self.backbone(x)
         age = self.age_fc(age.unsqueeze(1))
 
         gender = self.gender_fc(gender.unsqueeze(1))
         weight = self.weight_fc(weight.unsqueeze(1))
-        hight = self.hight_fc(hight.unsqueeze(1))
+        height = self.height_fc(height.unsqueeze(1))
         out_all = torch.cat([out, age.unsqueeze(2).unsqueeze(3).repeat(1, 1, 512, 512)], dim=1)
         out_all = torch.cat([out_all, gender.unsqueeze(2).unsqueeze(3).repeat(1, 1, 512, 512)], dim=1)
         out_all = torch.cat([out_all, weight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 512, 512)], dim=1)
-        out_all = torch.cat([out_all, hight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 512, 512)], dim=1)
+        out_all = torch.cat([out_all, height.unsqueeze(2).unsqueeze(3).repeat(1, 1, 512, 512)], dim=1)
 
         out_segment = self.decoder(out_all)
         out_down = self.downsampling(out)
@@ -193,10 +193,10 @@ class MultiModal(nn.Module):
         out_age = self.branch_age(out_flatten)
         out_gender = self.branch_gender(out_flatten)
         out_weight = self.branch_weight(out_flatten)
-        out_hight = self.branch_hight(out_flatten)
+        out_height = self.branch_height(out_flatten)
 
         
-        return  out_segment, out_age, out_gender, out_weight, out_hight
+        return  out_segment, out_age, out_gender, out_weight, out_height
     
 class MultiModalV2(nn.Module):
     def __init__(self, in_features=29, encoder = 'densenet169'):
@@ -207,7 +207,7 @@ class MultiModalV2(nn.Module):
         self.age_fc = nn.Linear(1, in_features)
         self.gender_fc = nn.Linear(2, in_features)
         self.weight_fc = nn.Linear(1, in_features)
-        self.hight_fc = nn.Linear(1, in_features)
+        self.height_fc = nn.Linear(1, in_features)
         
         # self.downsampling = nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1)
 
@@ -238,7 +238,7 @@ class MultiModalV2(nn.Module):
                 nn.Linear(51,1),
                 nn.Softmax()
             )
-        self.branch_hight = nn.Sequential(
+        self.branch_height = nn.Sequential(
                 nn.Conv2d(in_features, in_features, kernel_size=5, stride=5, padding=1),
                 nn.Flatten(),
                 nn.Linear(in_features=19604, out_features=51),
@@ -259,19 +259,19 @@ class MultiModalV2(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(in_features, 29, kernel_size=1, stride=1),
         )
-    def forward(self, x, age, gender, weight, hight):
+    def forward(self, x, age, gender, weight, height):
         out = self.backbone(x)
         age = self.age_fc(age)
 
         gender = self.gender_fc(gender)
 
         weight = self.weight_fc(weight)
-        hight = self.hight_fc(hight)
+        height = self.height_fc(height)
 
         out_all = torch.cat([out, age.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
         out_all = torch.cat([out_all, gender.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
         out_all = torch.cat([out_all, weight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
-        out_all = torch.cat([out_all, hight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
+        out_all = torch.cat([out_all, height.unsqueeze(2).unsqueeze(3).repeat(1, 1, 128, 128)], dim=1)
 
         out_segment = self.decoder(out_all)
         #out_down = self.downsampling(out)
@@ -280,9 +280,9 @@ class MultiModalV2(nn.Module):
         out_age = self.branch_age(out) # out_flatten -> out
         out_gender = self.branch_gender(out) # out_flatten -> out
         out_weight = self.branch_weight(out) # out_flatten -> out
-        out_hight = self.branch_hight(out) # out_flatten -> out
+        out_height = self.branch_height(out) # out_flatten -> out
         
-        return  out_segment, out_age, out_gender, out_weight, out_hight
+        return  out_segment, out_age, out_gender, out_weight, out_height
     
 
 class MultiModalV3(nn.Module):
@@ -295,7 +295,7 @@ class MultiModalV3(nn.Module):
         self.branch_age = nn.Sequential(
                 nn.Flatten(),
                 nn.Linear(in_features,1),
-                nn.Softmax()
+                nn.Sigmoid()
             )
         self.branch_gender = nn.Sequential(
                 nn.Flatten(),
@@ -305,12 +305,12 @@ class MultiModalV3(nn.Module):
         self.branch_weight = nn.Sequential(
                 nn.Flatten(),
                 nn.Linear(in_features,1),
-                nn.Softmax()
+                nn.Sigmoid()
             )
-        self.branch_hight = nn.Sequential(
+        self.branch_height = nn.Sequential(
                 nn.Flatten(),
                 nn.Linear(in_features,1),
-                nn.Softmax()
+                nn.Sigmoid()
             )
 
         self.decoder = nn.Sequential(
@@ -321,12 +321,12 @@ class MultiModalV3(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(in_features+5, in_features, kernel_size=3, stride=1, padding=1)
         )
-    def forward(self, x, age, gender, weight, hight):
+    def forward(self, x, age, gender, weight, height):
         out = self.backbone(x)
         out_all = torch.cat([out, age.unsqueeze(2).unsqueeze(3).repeat(1, 1, 256, 256)], dim=1)
         out_all = torch.cat([out_all, gender.unsqueeze(2).unsqueeze(3).repeat(1, 1, 256, 256)], dim=1)
         out_all = torch.cat([out_all, weight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 256, 256)], dim=1)
-        out_all = torch.cat([out_all, hight.unsqueeze(2).unsqueeze(3).repeat(1, 1, 256, 256)], dim=1)
+        out_all = torch.cat([out_all, height.unsqueeze(2).unsqueeze(3).repeat(1, 1, 256, 256)], dim=1)
 
         out_segment = self.decoder(out_all)
 
@@ -335,6 +335,6 @@ class MultiModalV3(nn.Module):
         out_age = self.branch_age(ap) # out_flatten -> out
         out_gender = self.branch_gender(ap) # out_flatten -> out
         out_weight = self.branch_weight(ap) # out_flatten -> out
-        out_hight = self.branch_hight(ap) # out_flatten -> out
+        out_height = self.branch_height(ap) # out_flatten -> out
         
-        return  out_segment, out_age, out_gender, out_weight, out_hight
+        return  out_segment, out_age, out_gender, out_weight, out_height
