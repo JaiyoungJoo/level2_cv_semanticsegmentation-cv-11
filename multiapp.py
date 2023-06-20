350246e4847fe66305ff7091985a887e65b72b85
Yoonpyo Lee <69185594+imsmile2000@users.noreply.github.com>
2023-06-21 02:10:59 +0900

    [Fix] Functionr -> Function

--- multiapp.py
+++ multiapp.py
@@ -40,7 +40,7 @@ class MultiApp:
 
     def run(self):
             app = st.sidebar.radio(
-                'Fucntionr',
+                'Fucntion',
                 self.apps,
                 format_func=lambda app: app['title'])
 
